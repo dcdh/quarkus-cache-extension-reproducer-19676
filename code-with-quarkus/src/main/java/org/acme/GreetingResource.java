ee8a476fce6342806060ca290ba7b877ab83ba92
damien <damien.clementdhuart@gmail.com>
2026-01-04 08:18:38 +0100

    ok not this way ...

--- code-with-quarkus/src/main/java/org/acme/GreetingResource.java
+++ code-with-quarkus/src/main/java/org/acme/GreetingResource.java
@@ -1,17 +1,23 @@
 package org.acme;
 
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.acme.greeting.extension.runtime.WeatherForecastService;
+
+import java.time.LocalDate;
 
 @Path("/hello")
 public class GreetingResource {
 
+    @Inject
+    WeatherForecastService weatherForecastService;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-//        FCK faire le necessaire pour appeler un service utilisant le cache ...
-        return "Hello from Quarkus REST";
+        return weatherForecastService.getDailyForecast(LocalDate.of(2026, 1, 1), "Paris");
     }
 }
--- my-extension-using-cache/deployment/src/main/java/org/acme/greeting/extension/deployment/MyExtensionUsingCacheExtensionProcessor.java
+++ my-extension-using-cache/deployment/src/main/java/org/acme/greeting/extension/deployment/MyExtensionUsingCacheExtensionProcessor.java
@@ -1,7 +1,9 @@
 package org.acme.greeting.extension.deployment;
 
+import io.quarkus.arc.deployment.AdditionalBeanBuildItem;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
+import org.acme.greeting.extension.runtime.WeatherForecastService;
 
 class MyExtensionUsingCacheExtensionProcessor {
 
@@ -10,5 +12,10 @@ class MyExtensionUsingCacheExtensionProcessor {
     @BuildStep
     FeatureBuildItem feature() {
         return new FeatureBuildItem(FEATURE);
+    }
+
+    @BuildStep
+    AdditionalBeanBuildItem additionalBeanBuildItemProducer() {
+        return AdditionalBeanBuildItem.builder().addBeanClass(WeatherForecastService.class).build();
     }
 }
